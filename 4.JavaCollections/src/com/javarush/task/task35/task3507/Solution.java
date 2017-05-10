67dfe65b75dfb77b86144531b98fc9db80d7a442
astronarh <astronarh@gmail.com>
2017-05-10 15:25:46 +0300

    Добавлено считывание файлов с контролем имени файла

--- 4.JavaCollections/src/com/javarush/task/task35/task3507/Solution.java
+++ 4.JavaCollections/src/com/javarush/task/task35/task3507/Solution.java
@@ -1,5 +1,7 @@
 package com.javarush.task.task35.task3507;
 
+import java.io.File;
+import java.io.FilenameFilter;
 import java.util.Set;
 
 /* 
@@ -12,6 +14,32 @@ public class Solution {
     }
 
     public static Set<? extends Animal> getAllAnimals(String pathToAnimals) {
+        String ext = ".class";
+        File file = new File(pathToAnimals);
+        if(!file.exists()) System.out.println(pathToAnimals + " папка не существует");
+        File[] listFiles = file.listFiles(new MyFileNameFilter(ext));
+        if(listFiles.length == 0){
+            System.out.println(pathToAnimals + " не содержит файлов с расширением " + ext);
+        }else{
+            for(File f : listFiles)
+                System.out.println("Файл: " + pathToAnimals + File.separator + f.getName());
+        }
+
+
+
         return null;
+    }
+
+    public static class MyFileNameFilter implements FilenameFilter {
+
+        private String ext;
+
+        public MyFileNameFilter(String ext){
+            this.ext = ext.toLowerCase();
+        }
+        @Override
+        public boolean accept(File dir, String name) {
+            return name.toLowerCase().endsWith(ext);
+        }
     }
 }
