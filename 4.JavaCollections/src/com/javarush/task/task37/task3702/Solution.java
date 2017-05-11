638d7d3baa39f2faff9ea5aab88bfecc203f48d0
astronarh <astronarh@gmail.com>
2017-05-11 10:24:13 +0300

    2 этап пройден

--- 4.JavaCollections/src/com/javarush/task/task37/task3702/Solution.java
+++ 4.JavaCollections/src/com/javarush/task/task37/task3702/Solution.java
@@ -1,10 +1,16 @@
 package com.javarush.task.task37.task3702;
 
+import com.javarush.task.task37.task3702.male.MaleFactory;
+
 /**
  * Created by ShkerdinVA on 11.05.2017.
  */
 public class Solution {
     public static void main(String... args) {
+        MaleFactory maleFactory = new MaleFactory();
+        System.out.println(maleFactory.getPerson(99));
+        System.out.println(maleFactory.getPerson(4));
+        System.out.println(maleFactory.getPerson(15));
 
     }
 }
