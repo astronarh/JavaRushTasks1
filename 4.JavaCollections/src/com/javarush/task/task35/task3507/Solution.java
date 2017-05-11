d7a4ca46c5f81cd876840998895c4cdb3613a045
astronarh <astronarh@gmail.com>
2017-05-11 10:07:44 +0300

    Добавление людей и интерфейса для них

--- 4.JavaCollections/src/com/javarush/task/task35/task3507/Solution.java
+++ 4.JavaCollections/src/com/javarush/task/task35/task3507/Solution.java
@@ -7,10 +7,10 @@ import com.sun.org.apache.bcel.internal.util.ClassLoader;
 import java.io.*;
 import java.lang.reflect.Constructor;
 import java.lang.reflect.Modifier;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
+import java.net.URI;
+import java.nio.file.*;
 import java.util.HashSet;
+import java.util.Iterator;
 import java.util.Set;
 
 /* 
@@ -26,6 +26,137 @@ public class Solution {
         Set<Animal> result = new HashSet<>();
         File dir = new File(pathToAnimals);
 
+        Path path = new Path() {
+            @Override
+            public FileSystem getFileSystem() {
+                return null;
+            }
+
+            @Override
+            public boolean isAbsolute() {
+                return false;
+            }
+
+            @Override
+            public Path getRoot() {
+                return null;
+            }
+
+            @Override
+            public Path getFileName() {
+                return null;
+            }
+
+            @Override
+            public Path getParent() {
+                return null;
+            }
+
+            @Override
+            public int getNameCount() {
+                return 0;
+            }
+
+            @Override
+            public Path getName(int index) {
+                return null;
+            }
+
+            @Override
+            public Path subpath(int beginIndex, int endIndex) {
+                return null;
+            }
+
+            @Override
+            public boolean startsWith(Path other) {
+                return false;
+            }
+
+            @Override
+            public boolean startsWith(String other) {
+                return false;
+            }
+
+            @Override
+            public boolean endsWith(Path other) {
+                return false;
+            }
+
+            @Override
+            public boolean endsWith(String other) {
+                return false;
+            }
+
+            @Override
+            public Path normalize() {
+                return null;
+            }
+
+            @Override
+            public Path resolve(Path other) {
+                return null;
+            }
+
+            @Override
+            public Path resolve(String other) {
+                return null;
+            }
+
+            @Override
+            public Path resolveSibling(Path other) {
+                return null;
+            }
+
+            @Override
+            public Path resolveSibling(String other) {
+                return null;
+            }
+
+            @Override
+            public Path relativize(Path other) {
+                return null;
+            }
+
+            @Override
+            public URI toUri() {
+                return null;
+            }
+
+            @Override
+            public Path toAbsolutePath() {
+                return null;
+            }
+
+            @Override
+            public Path toRealPath(LinkOption... options) throws IOException {
+                return null;
+            }
+
+            @Override
+            public File toFile() {
+                return null;
+            }
+
+            @Override
+            public WatchKey register(WatchService watcher, WatchEvent.Kind<?>[] events, WatchEvent.Modifier... modifiers) throws IOException {
+                return null;
+            }
+
+            @Override
+            public WatchKey register(WatchService watcher, WatchEvent.Kind<?>[] events) throws IOException {
+                return null;
+            }
+
+            @Override
+            public Iterator<Path> iterator() {
+                return null;
+            }
+
+            @Override
+            public int compareTo(Path other) {
+                return 0;
+            }
+        };
         File[] files = getClassFiles(path);
 
         try {
---
+++ 4.JavaCollections/src/com/javarush/task/task37/task3702/male/KidBoy.java
@@ -0,0 +1,9 @@
+package com.javarush.task.task37.task3702.male;
+
+import com.javarush.task.task37.task3702.Human;
+
+/**
+ * Created by ShkerdinVA on 11.05.2017.
+ */
+public class KidBoy implements Human {
+}
