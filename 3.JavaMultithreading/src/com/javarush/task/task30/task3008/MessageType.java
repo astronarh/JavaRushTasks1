6d9c89e497c1c43de2a01709a92a4f3611417903
astronarh <astronarh@gmail.com>
2017-05-12 17:36:49 +0300

    Add enums in MessageType

--- 3.JavaMultithreading/src/com/javarush/task/task30/task3008/MessageType.java
+++ 3.JavaMultithreading/src/com/javarush/task/task30/task3008/MessageType.java
@@ -4,4 +4,10 @@ package com.javarush.task.task30.task3008;
  * Created by ShkerdinVA on 12.05.2017.
  */
 public enum MessageType {
+    NAME_REQUEST,
+    USER_NAME,
+    NAME_ACCEPTED,
+    TEXT,
+    USER_ADDED,
+    USER_REMOVED
 }
