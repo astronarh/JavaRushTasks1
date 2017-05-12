a79c7dd2a850f5902eb671cc93fbe3842302f365
astronarh <astronarh@gmail.com>
2017-05-12 17:28:13 +0300

    Add logic in ConsoleHelper

--- 3.JavaMultithreading/src/com/javarush/task/task30/task3008/ConsoleHelper.java
+++ 3.JavaMultithreading/src/com/javarush/task/task30/task3008/ConsoleHelper.java
@@ -1,7 +1,42 @@
 package com.javarush.task.task30.task3008;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 /**
  * Created by ShkerdinVA on 12.05.2017.
  */
 public class ConsoleHelper {
+    private static BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+    static void writeMessage(String message) {
+        System.out.println(message);
+    }
+
+    static String readString() {
+        String outString;
+        while (true) {
+            try {
+                outString = bufferedReader.readLine();
+                break;
+            } catch (IOException e) {
+                System.out.println("Произошла ошибка при попытке ввода текста. Попробуйте еще раз.");
+            }
+        }
+        return outString;
+    }
+
+    static int readInt() {
+        int number;
+        while (true) {
+            try {
+                number = Integer.parseInt(readString());
+                break;
+            } catch (NumberFormatException e) {
+                System.out.println("Произошла ошибка при попытке ввода текста. Попробуйте еще раз.");
+            }
+        }
+        return number;
+    }
 }
