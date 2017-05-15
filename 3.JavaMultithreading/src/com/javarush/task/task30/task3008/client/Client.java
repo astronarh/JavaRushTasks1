920ed4c50f5a4c2b089feeed89df10cbca570355
astronarh <astronarh@gmail.com>
2017-05-15 17:36:12 +0300

    Fucking bullshit, infuriates bitch

--- 3.JavaMultithreading/src/com/javarush/task/task30/task3008/client/Client.java
+++ 3.JavaMultithreading/src/com/javarush/task/task30/task3008/client/Client.java
@@ -3,6 +3,7 @@ package com.javarush.task.task30.task3008.client;
 import com.javarush.task.task30.task3008.Connection;
 import com.javarush.task.task30.task3008.ConsoleHelper;
 import com.javarush.task.task30.task3008.Message;
+import com.javarush.task.task30.task3008.MessageType;
 
 import java.io.IOException;
 
@@ -62,6 +63,36 @@ public class Client {
     }
 
     public class SocketThread extends Thread {
+        protected void clientHandshake() throws IOException, ClassNotFoundException {
+            while(true) {
+                Message message = connection.receive();
+                if (message.getType() == MessageType.NAME_REQUEST) {
+                    Message m = new Message(MessageType.USER_NAME, getUserName());
+                    connection.send(m);
+                } else if (message.getType() == MessageType.NAME_ACCEPTED) {
+                    notifyConnectionStatusChanged(true);
+                    return;
+                } else {
+                    throw new IOException("Unexpected MessageType");
+                }
+            }
+        }
+
+        protected void clientMainLoop() throws IOException, ClassNotFoundException {
+            while (true) {
+                Message message = connection.receive();
+                if (message.getType() == MessageType.TEXT) {
+                    processIncomingMessage(message.getData());
+                } else if (message.getType() == MessageType.USER_ADDED) {
+                    informAboutAddingNewUser(message.getData());
+                } else if (message.getType() == MessageType.USER_REMOVED) {
+                    informAboutDeletingNewUser(message.getData());
+                } else {
+                    throw new IOException("Unexpected MessageType");
+                }
+            }
+        }
+
         protected void processIncomingMessage(String message) {
             ConsoleHelper.writeMessage(message);
         }
