c0865c21ae1ca6a2d9cf9ee7c9ff0d6e986edc90
astronarh <astronarh@gmail.com>
2017-05-15 11:37:48 +0300

    Implemented the logic of shaking hands

--- 3.JavaMultithreading/src/com/javarush/task/task30/task3008/Server.java
+++ 3.JavaMultithreading/src/com/javarush/task/task30/task3008/Server.java
@@ -29,6 +29,22 @@ public class Server {
         public Handler(Socket socket) {
             this.socket = socket;
         }
+
+        private String serverHandshake(Connection connection)  throws IOException, ClassNotFoundException{
+            while (true) {
+                connection.send(new Message(MessageType.NAME_REQUEST));
+                Message message = connection.receive();
+                if (message.getType() == MessageType.USER_NAME) {
+                    if (!message.getData().isEmpty()) {
+                        if (!connectionMap.containsKey(message.getData())) {
+                            connectionMap.put(message.getData(), connection);
+                            connection.send(new Message(MessageType.NAME_ACCEPTED));
+                            return message.getData();
+                        }
+                    }
+                }
+            }
+        }
     }
 
     public static void main(String ...args) {
