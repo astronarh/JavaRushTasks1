1e6a12919904189d4f0e2a134e278235bdf497eb
astronarh <astronarh@gmail.com>
2017-05-15 14:51:37 +0300

    Add client package and Client class

---
+++ 3.JavaMultithreading/src/com/javarush/task/task30/task3008/client/Client.java
@@ -0,0 +1,15 @@
+package com.javarush.task.task30.task3008.client;
+
+import com.javarush.task.task30.task3008.Connection;
+
+/**
+ * Created by ShkerdinVA on 15.05.2017.
+ */
+public class Client {
+    protected Connection connection;
+    private volatile boolean clientConnected = false;
+
+    public class SocketThread extends Thread {
+
+    }
+}
