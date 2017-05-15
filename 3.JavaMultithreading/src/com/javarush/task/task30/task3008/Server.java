f143f68a739ab10ae94eab08c4fa5e72ec8c0f96
astronarh <astronarh@gmail.com>
2017-05-15 10:24:52 +0300

    Add logic in Server

--- 3.JavaMultithreading/src/com/javarush/task/task30/task3008/Server.java
+++ 3.JavaMultithreading/src/com/javarush/task/task30/task3008/Server.java
@@ -1,10 +1,33 @@
 package com.javarush.task.task30.task3008;
 
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 /**
  * Created by ShkerdinVA on 12.05.2017.
  */
 public class Server {
-    public static void main(String ...args) {
+    private static class Handler extends Thread {
+        private Socket socket;
 
+        public Handler(Socket socket) {
+            this.socket = socket;
+        }
+    }
+
+    public static void main(String ...args) {
+        ConsoleHelper.writeMessage("Введите порт сервера... ");
+        int port = ConsoleHelper.readInt();
+        try (ServerSocket serverSocket = new ServerSocket(port)) {
+            ConsoleHelper.writeMessage("Серввер запущен.");
+            while (true) {
+                Socket socket = serverSocket.accept();
+                Handler handler = new Handler(socket);
+                handler.start();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
