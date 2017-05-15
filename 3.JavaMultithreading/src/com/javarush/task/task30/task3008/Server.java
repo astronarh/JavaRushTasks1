bf6f9c40b54bd083897933d8f25768704f7ce1bc
astronarh <astronarh@gmail.com>
2017-05-15 11:50:01 +0300

    Implemented sending all clients to all customers

--- 3.JavaMultithreading/src/com/javarush/task/task30/task3008/Server.java
+++ 3.JavaMultithreading/src/com/javarush/task/task30/task3008/Server.java
@@ -45,6 +45,15 @@ public class Server {
                 }
             }
         }
+
+        private void sendListOfUsers(Connection connection, String userName) throws IOException {
+            for (String x : connectionMap.keySet()) {
+                Message message = new Message(MessageType.USER_ADDED, x);
+                if (!x.equals(userName)) {
+                    connection.send(message);
+                }
+            }
+        }
     }
 
     public static void main(String ...args) {
