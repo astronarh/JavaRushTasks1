8f49914788030dbc5f6ec3ce71dd76a6120d42c4
astronarh <astronarh@gmail.com>
2017-05-17 16:35:25 +0300

    Done

---
+++ 4.JavaCollections/src/com/javarush/task/task33/task3310/Shortener.java
@@ -0,0 +1,15 @@
+package com.javarush.task.task33.task3310;
+
+/**
+ * Created by ShkerdinVA on 17.05.2017.
+ */
+public class Shortener {
+    public Long getId(String string) {
+        Long theLong = new Long(0);
+        return theLong;
+    }
+
+    public String getString(Long id) {
+        return null;
+    }
+}
---
+++ 4.JavaCollections/src/com/javarush/task/task33/task3310/Solution.java
@@ -0,0 +1,10 @@
+package com.javarush.task.task33.task3310;
+
+/**
+ * Created by ShkerdinVA on 17.05.2017.
+ */
+public class Solution {
+    public static void main(String... args) {
+
+    }
+}
