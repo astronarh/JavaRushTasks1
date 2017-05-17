9742b0edde5b98503b96f1672445d0200380fa39
astronarh <astronarh@gmail.com>
2017-05-17 17:13:33 +0300

    Done

---
+++ 4.JavaCollections/src/com/javarush/task/task33/task3310/ExceptionHandler.java
@@ -0,0 +1,10 @@
+package com.javarush.task.task33.task3310;
+
+/**
+ * Created by ShkerdinVA on 17.05.2017.
+ */
+public class ExceptionHandler {
+    static void log(Exception e) {
+        System.out.println(e);
+    }
+}
