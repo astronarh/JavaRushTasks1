4da645b470275dec6bf48e5aa033a2c58238b0c3
astronarh <astronarh@gmail.com>
2017-05-23 16:16:01 +0300

    Step 5 is completed

--- 3.JavaMultithreading/src/com/javarush/task/task31/task3110/command/ExitCommand.java
+++ 3.JavaMultithreading/src/com/javarush/task/task31/task3110/command/ExitCommand.java
@@ -2,10 +2,7 @@ package com.javarush.task.task31.task3110.command;
 
 import com.javarush.task.task31.task3110.ConsoleHelper;
 
-/**
- * Created by ShkerdinVA on 23.05.2017.
- */
-public class ExitCommand implements Command{
+public class ExitCommand implements Command {
     @Override
     public void execute() throws Exception {
         ConsoleHelper.writeMessage("До встречи!");
