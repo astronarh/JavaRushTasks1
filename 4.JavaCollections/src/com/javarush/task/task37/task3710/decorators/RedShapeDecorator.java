1767c64f117d534d4b58c9c16415f3bce781085a
astronarh <astronarh@gmail.com>
2017-05-25 09:57:41 +0300

    Done

---
+++ 4.JavaCollections/src/com/javarush/task/task37/task3710/decorators/RedShapeDecorator.java
@@ -0,0 +1,25 @@
+package com.javarush.task.task37.task3710.decorators;
+
+import com.javarush.task.task37.task3710.shapes.Shape;
+
+/**
+ * Created by ShkerdinVA on 25.05.2017.
+ */
+public class RedShapeDecorator extends ShapeDecorator{
+    private Shape shape;
+
+    public RedShapeDecorator(Shape decoratedShape) {
+        super(decoratedShape);
+        this.shape = decoratedShape;
+    }
+
+    private void setBorderColor(Shape shape) {
+        System.out.println("Setting border color for " + shape.getClass().getSimpleName() + " to red.");
+    }
+
+    @Override
+    public void draw() {
+        setBorderColor(shape);
+        super.draw();
+    }
+}
