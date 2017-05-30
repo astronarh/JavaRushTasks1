cbf74bbe9717e0b9890fa8953d832d8b1feb73ae
astronarh <astronarh@gmail.com>
2017-05-30 13:53:49 +0300

    Done

--- 4.JavaCollections/src/com/javarush/task/task39/task3913/Solution.java
+++ 4.JavaCollections/src/com/javarush/task/task39/task3913/Solution.java
@@ -8,5 +8,9 @@ public class Solution {
         LogParser logParser = new LogParser(Paths.get("D:\\JavaRush\\JavaRushTasks\\4.JavaCollections\\src\\com\\javarush\\task\\task39\\task3913\\logs"));
         System.out.println(logParser.getNumberOfUniqueIPs(null, new Date()));
         System.out.println(logParser.getNumberOfUserEvents("Eduard Petrovich Morozko", null, null));
+        System.out.println(logParser.getDateWhenUserLoggedFirstTime("Eduard Petrovich Morozko", null, null));
+        System.out.println(logParser.getDateWhenUserSolvedTask("Amigo", 18, null, null));
+        System.out.println(logParser.getDateWhenUserDoneTask("Eduard Petrovich Morozko", 48, null, null));
+
     }
 }
