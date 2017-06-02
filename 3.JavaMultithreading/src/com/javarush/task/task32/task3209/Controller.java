0503ee2d3a4c8b6ed45557d52fcb8e4b7e0af6fb
astronarh <astronarh@gmail.com>
2017-06-02 17:25:34 +0300

    Path 19 done

--- 3.JavaMultithreading/src/com/javarush/task/task32/task3209/Controller.java
+++ 3.JavaMultithreading/src/com/javarush/task/task32/task3209/Controller.java
@@ -66,4 +66,16 @@ public class Controller {
         }
         return stringWriter.toString();
     }
+
+    public void createNewDocument() {
+    }
+
+    public void openDocument() {
+    }
+
+    public void saveDocument() {
+    }
+
+    public void saveDocumentAs() {
+    }
 }
