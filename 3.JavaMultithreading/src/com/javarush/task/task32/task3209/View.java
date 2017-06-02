36f96916bac956418ce4a5b0f00907c5a8a5f42c
astronarh <astronarh@gmail.com>
2017-06-02 15:28:23 +0300

    Path 13 done

--- 3.JavaMultithreading/src/com/javarush/task/task32/task3209/View.java
+++ 3.JavaMultithreading/src/com/javarush/task/task32/task3209/View.java
@@ -122,4 +122,8 @@ public class View extends JFrame implements ActionListener {
     public void resetUndo() {
         undoManager.discardAllEdits();
     }
+
+    public boolean isHtmlTabSelected() {
+        return tabbedPane.getSelectedIndex() == 0;
+    }
 }
--- 3.JavaMultithreading/src/com/javarush/task/task32/task3209/listeners/TextEditMenuListener.java
+++ 3.JavaMultithreading/src/com/javarush/task/task32/task3209/listeners/TextEditMenuListener.java
@@ -2,19 +2,26 @@ package com.javarush.task.task32.task3209.listeners;
 
 import com.javarush.task.task32.task3209.View;
 
+import javax.swing.*;
 import javax.swing.event.MenuEvent;
 import javax.swing.event.MenuListener;
+import java.awt.*;
 
 /**
  * Created by ShkerdinVA on 02.06.2017.
  */
 public class TextEditMenuListener implements MenuListener {
+    private View view;
+
     public TextEditMenuListener(View view) {
+        this.view = view;
     }
 
     @Override
     public void menuSelected(MenuEvent e) {
-
+        JMenu jMenu = (JMenu) e.getSource();
+        Component[] components = jMenu.getMenuComponents();
+        for (Component component : components) component.setEnabled(view.isHtmlTabSelected());
     }
 
     @Override
@@ -25,5 +32,9 @@ public class TextEditMenuListener implements MenuListener {
     @Override
     public void menuCanceled(MenuEvent e) {
 
+    }
+
+    public boolean isHtmlTabSelected() {
+        return view.areFocusTraversalKeysSet(0);
     }
 }
