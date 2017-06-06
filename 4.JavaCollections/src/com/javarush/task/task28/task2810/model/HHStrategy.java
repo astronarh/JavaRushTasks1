815c7fbde1fd3c7e45827f07217424c6bbde0a93
astronarh <astronarh@gmail.com>
2017-06-06 15:55:29 +0300

    Aggregator path 4 done

--- 4.JavaCollections/src/com/javarush/task/task28/task2810/model/HHStrategy.java
+++ 4.JavaCollections/src/com/javarush/task/task28/task2810/model/HHStrategy.java
@@ -4,4 +4,7 @@ package com.javarush.task.task28.task2810.model;
  * Created by ShkerdinVA on 06.06.2017.
  */
 public class HHStrategy implements Strategy{
+    private static final String URL_FORMAT = "http://hh.ua/search/vacancy?text=java+%s&page=%d";
+
+
 }
