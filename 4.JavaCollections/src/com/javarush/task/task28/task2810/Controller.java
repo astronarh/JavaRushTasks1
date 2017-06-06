9e4f105b62b06ff42c878d6d2594760092b627d4
astronarh <astronarh@gmail.com>
2017-06-06 16:22:20 +0300

    Aggregator path 5 done

--- 4.JavaCollections/src/com/javarush/task/task28/task2810/Controller.java
+++ 4.JavaCollections/src/com/javarush/task/task28/task2810/Controller.java
@@ -1,8 +1,11 @@
 package com.javarush.task.task28.task2810;
 
 import com.javarush.task.task28.task2810.model.Provider;
+import com.javarush.task.task28.task2810.vo.Vacancy;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 /**
  * Created by ShkerdinVA on 06.06.2017.
@@ -20,5 +23,20 @@ public class Controller {
     public Controller(Provider... providers) {
         if (providers.length == 0) throw new IllegalArgumentException();
         else this.providers = providers;
+    }
+
+    public void scan() {
+        try {
+            List<Vacancy> list = new ArrayList<>();
+            for (Provider provider : providers) {
+                List<Vacancy> listVacPr = provider.getJavaVacancies("???");
+                for (Vacancy vac : listVacPr) {
+                    list.add(vac);
+                }
+            }
+            System.out.println(list.size());
+        } catch (NullPointerException e) {
+            System.out.println(0);
+        }
     }
 }
--- 4.JavaCollections/src/com/javarush/task/task28/task2810/model/HHStrategy.java
+++ 4.JavaCollections/src/com/javarush/task/task28/task2810/model/HHStrategy.java
@@ -1,5 +1,9 @@
 package com.javarush.task.task28.task2810.model;
 
+import com.javarush.task.task28.task2810.vo.Vacancy;
+
+import java.util.List;
+
 /**
  * Created by ShkerdinVA on 06.06.2017.
  */
@@ -7,4 +11,8 @@ public class HHStrategy implements Strategy{
     private static final String URL_FORMAT = "http://hh.ua/search/vacancy?text=java+%s&page=%d";
 
 
+    @Override
+    public List<Vacancy> getVacancies(String searchString) {
+        return null;
+    }
 }
