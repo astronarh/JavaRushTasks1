d6c7d497508fa10a8e706ade498357f3eb2df600
astronarh <astronarh@gmail.com>
2017-06-07 16:59:10 +0300

    Aggregator path 16 done

--- 4.JavaCollections/src/com/javarush/task/task28/task2810/Aggregator.java
+++ 4.JavaCollections/src/com/javarush/task/task28/task2810/Aggregator.java
@@ -2,6 +2,7 @@ package com.javarush.task.task28.task2810;
 
 import com.javarush.task.task28.task2810.model.HHStrategy;
 import com.javarush.task.task28.task2810.model.Model;
+import com.javarush.task.task28.task2810.model.MoikrugStrategy;
 import com.javarush.task.task28.task2810.model.Provider;
 import com.javarush.task.task28.task2810.view.HtmlView;
 
@@ -14,9 +15,12 @@ public class Aggregator {
         Controller controller = new Controller(provider);
         controller.scan();*/
         HtmlView view = new HtmlView();
-        Model model = new Model(view, new Provider[]{new Provider(new HHStrategy())});
+        Provider hhProvider = new Provider(new HHStrategy());
+        Provider moikrugProvider = new Provider(new MoikrugStrategy());
+        Model model = new Model(view, hhProvider, moikrugProvider);
+        //Model model = new Model(view, hhProvider);
         Controller controller = new Controller(model);
         view.setController(controller);
-        view.userCitySelectEmulationMethod();
+        view.userCitySelectEmulationMethod ();
     }
 }
