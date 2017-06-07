ee31b78e1ae8ad98d56452a740cf6a3367c1baa9
astronarh <astronarh@gmail.com>
2017-06-07 16:41:00 +0300

    Aggregator path 15 done

--- 4.JavaCollections/src/com/javarush/task/task28/task2810/view/HtmlView.java
+++ 4.JavaCollections/src/com/javarush/task/task28/task2810/view/HtmlView.java
@@ -2,8 +2,12 @@ package com.javarush.task.task28.task2810.view;
 
 import com.javarush.task.task28.task2810.Controller;
 import com.javarush.task.task28.task2810.vo.Vacancy;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
 
 import java.io.BufferedWriter;
+import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.List;
@@ -31,7 +35,29 @@ public class HtmlView implements View{
     }
 
     private String getUpdatedFileContent(List<Vacancy> list) {
-        return "";
+        Document doc = null;
+        try {
+            doc = getDocument();
+            Element element = doc.getElementsByClass("template").first();
+            Element cloneElement = element.clone();
+            cloneElement.removeClass("template").removeAttr("style");
+            doc.getElementsByAttributeValue("class","vacancy").remove();
+            for (Vacancy vacancy : list)
+            {
+                Element vac = cloneElement.clone();
+                vac.getElementsByAttributeValue("class", "city").get(0).text(vacancy.getCity());
+                vac.getElementsByAttributeValue("class", "companyName").get(0).text(vacancy.getCompanyName());
+                vac.getElementsByAttributeValue("class", "salary").get(0).text(vacancy.getSalary());
+                Element link = vac.getElementsByTag("a").get(0);
+                link.text(vacancy.getTitle());
+                link.attr("href", vacancy.getUrl());
+                element.before(vac.outerHtml());
+            }
+            return doc.html();
+        } catch (IOException e) {
+            e.printStackTrace();
+            return "Some exception occurred";
+        }
     }
 
     private void updateFile(String string) {
@@ -41,5 +67,9 @@ public class HtmlView implements View{
         catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    protected Document getDocument() throws IOException {
+        return Jsoup.parse(new File(filePath), "UTF-8");
     }
 }
