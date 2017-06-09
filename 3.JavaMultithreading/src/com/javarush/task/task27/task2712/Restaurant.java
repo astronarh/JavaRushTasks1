4bc484e44d5a7f527e0c8f4f6eb17964257cbb99
astronarh <astronarh@gmail.com>
2017-06-09 16:29:23 +0300

    Ресторан path 04 done

--- 3.JavaMultithreading/src/com/javarush/task/task27/task2712/Restaurant.java
+++ 3.JavaMultithreading/src/com/javarush/task/task27/task2712/Restaurant.java
@@ -1,6 +1,7 @@
 package com.javarush.task.task27.task2712;
 
 import com.javarush.task.task27.task2712.kitchen.Cook;
+import com.javarush.task.task27.task2712.kitchen.Waiter;
 
 /**
  * Created by ShkerdinVA on 09.06.2017.
@@ -9,7 +10,9 @@ public class Restaurant {
     public static void main(String... args) {
         Tablet tablet = new Tablet(5);
         Cook firstCook = new Cook("Amigo");
+        Waiter firstWaiter = new Waiter();
         tablet.addObserver(firstCook);
+        firstCook.addObserver(firstWaiter);
         tablet.createOrder();
     }
 }
