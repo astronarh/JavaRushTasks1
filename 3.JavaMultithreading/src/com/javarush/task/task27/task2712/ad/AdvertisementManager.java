b309a02b046dcfb7dfa58c77b33661f857f6a4e3
astronarh <astronarh@gmail.com>
2017-06-13 10:27:53 +0300

    Ресторан path 7 done

--- 3.JavaMultithreading/src/com/javarush/task/task27/task2712/ad/AdvertisementManager.java
+++ 3.JavaMultithreading/src/com/javarush/task/task27/task2712/ad/AdvertisementManager.java
@@ -4,4 +4,5 @@ package com.javarush.task.task27.task2712.ad;
  * Created by ShkerdinVA on 13.06.2017.
  */
 public class AdvertisementManager {
+    private final AdvertisementStorage storage = AdvertisementStorage.getInstance();
 }
