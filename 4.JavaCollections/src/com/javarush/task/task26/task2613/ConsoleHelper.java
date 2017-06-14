ed100d091e92bd2ede0feb4c38adfba37dbe29c5
astronarh <astronarh@gmail.com>
2017-06-14 17:53:21 +0300

    CashMachine (2) done

--- 4.JavaCollections/src/com/javarush/task/task26/task2613/ConsoleHelper.java
+++ 4.JavaCollections/src/com/javarush/task/task26/task2613/ConsoleHelper.java
@@ -1,7 +1,25 @@
 package com.javarush.task.task26.task2613;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 /**
  * Created by ShkerdinVA on 14.06.2017.
  */
 public class ConsoleHelper {
+    private static BufferedReader bis = new BufferedReader(new InputStreamReader(System.in));
+    public static void writeMessage(String message) {
+        System.out.println(message);
+    }
+
+    public static String readString() {
+        String input = "";
+        try {
+            input = bis.readLine();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return input;
+    }
 }
---
+++ 4.JavaCollections/src/com/javarush/task/task26/task2613/exception/InterruptOperationException.java
@@ -0,0 +1,7 @@
+package com.javarush.task.task26.task2613.exception;
+
+/**
+ * Created by ShkerdinVA on 14.06.2017.
+ */
+public class InterruptOperationException extends Exception {
+}
