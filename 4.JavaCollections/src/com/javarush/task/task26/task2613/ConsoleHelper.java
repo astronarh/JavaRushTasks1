7f849e74cf7f9b0279460511b84c482da88a44f8
astronarh <astronarh@gmail.com>
2017-06-14 17:42:01 +0300

    CashMachine (1) done

---
+++ 4.JavaCollections/src/com/javarush/task/task26/task2613/ConsoleHelper.java
@@ -0,0 +1,7 @@
+package com.javarush.task.task26.task2613;
+
+/**
+ * Created by ShkerdinVA on 14.06.2017.
+ */
+public class ConsoleHelper {
+}
---
+++ 4.JavaCollections/src/com/javarush/task/task26/task2613/Operation.java
@@ -0,0 +1,8 @@
+package com.javarush.task.task26.task2613;
+
+/**
+ * Created by ShkerdinVA on 14.06.2017.
+ */
+public enum Operation {
+    INFO, DEPOSIT, WITHDRAW, EXIT;
+}
