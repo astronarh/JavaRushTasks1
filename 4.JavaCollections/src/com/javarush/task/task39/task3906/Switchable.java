cdb93b74bdf0bd130977b633ff2929d8d60671b9
astronarh <astronarh@gmail.com>
2017-06-14 15:56:14 +0300

    Done

---
+++ 4.JavaCollections/src/com/javarush/task/task39/task3906/Switchable.java
@@ -0,0 +1,10 @@
+package com.javarush.task.task39.task3906;
+
+/**
+ * Created by ShkerdinVA on 14.06.2017.
+ */
+public interface Switchable {
+    boolean isOn();
+    void turnOff();
+    void turnOn();
+}
