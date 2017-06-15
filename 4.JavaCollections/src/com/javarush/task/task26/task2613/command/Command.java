a3cf5a3f179003b530181a11a45b2092448ac249
astronarh <astronarh@gmail.com>
2017-06-15 15:14:10 +0300

    CashMachine (7) done

---
+++ 4.JavaCollections/src/com/javarush/task/task26/task2613/command/Command.java
@@ -0,0 +1,8 @@
+package com.javarush.task.task26.task2613.command;
+
+/**
+ * Created by ShkerdinVA on 15.06.2017.
+ */
+interface Command {
+    void execute();
+}
---
+++ 4.JavaCollections/src/com/javarush/task/task26/task2613/command/WithdrawCommand.java
@@ -0,0 +1,11 @@
+package com.javarush.task.task26.task2613.command;
+
+/**
+ * Created by ShkerdinVA on 15.06.2017.
+ */
+class WithdrawCommand implements Command {
+    @Override
+    public void execute() {
+
+    }
+}
