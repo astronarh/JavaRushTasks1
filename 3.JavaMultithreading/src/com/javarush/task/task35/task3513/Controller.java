f7afae8cf4f91e08c2df30214096174d97328c6d
astronarh <astronarh@gmail.com>
2017-06-19 11:53:39 +0300

    2048 (8) done

--- 3.JavaMultithreading/src/com/javarush/task/task35/task3513/Controller.java
+++ 3.JavaMultithreading/src/com/javarush/task/task35/task3513/Controller.java
@@ -1,7 +1,19 @@
 package com.javarush.task.task35.task3513;
 
+import java.awt.event.KeyAdapter;
+
 /**
  * Created by ShkerdinVA on 16.06.2017.
  */
-public class Controller {
+public class Controller extends KeyAdapter{
+    private Model model;
+    private View view;
+
+    public Tile[][] getGameTiles() {
+        return model.getGameTiles();
+    }
+
+    public int getScore() {
+        return model.score;
+    }
 }
--- 3.JavaMultithreading/src/com/javarush/task/task35/task3513/Model.java
+++ 3.JavaMultithreading/src/com/javarush/task/task35/task3513/Model.java
@@ -12,6 +12,10 @@ public class Model {
     int score;
     int maxTile;
 
+    public Tile[][] getGameTiles() {
+        return gameTiles;
+    }
+
     public Model() {
         /*gameTiles = new Tile[FIELD_WIDTH][FIELD_WIDTH];
         for (int i = 0; i < FIELD_WIDTH; i++) {
@@ -19,6 +23,7 @@ public class Model {
                 gameTiles[i][j] = new Tile();
             }
         }*/
+
         resetGameTiles();
         this.score = 0;
         this.maxTile = 2;
@@ -134,5 +139,20 @@ public class Model {
         rotate();
         left();
         rotate();
+    }
+
+    public boolean canMove() {
+        if (!getEmptyTiles().isEmpty()) return true;
+        for (int i = 0; i < gameTiles.length; i++) {
+            for (int j = 1; j < gameTiles.length; j++) {
+                if (gameTiles[i][j].value == gameTiles[i][j - 1].value) return true;
+            }
+        }
+        for (int j = 0; j < gameTiles.length; j++) {
+            for (int i = 1; i < gameTiles.length; i++) {
+                if (gameTiles[i][j].value == gameTiles[i - 1][j].value) return true;
+            }
+        }
+        return false;
     }
 }
