91c7af4386a346e7dc704cabb439b27c87682b16
astronarh <astronarh@gmail.com>
2017-06-21 11:27:33 +0300

    "Sokoban (11)" done

--- 4.JavaCollections/src/com/javarush/task/task34/task3410/model/Model.java
+++ 4.JavaCollections/src/com/javarush/task/task34/task3410/model/Model.java
@@ -2,14 +2,36 @@ package com.javarush.task.task34.task3410.model;
 
 import com.javarush.task.task34.task3410.controller.EventListener;
 
+import java.nio.file.Paths;
+
 /**
  * Created by ShkerdinVA on 20.06.2017.
  */
 public class Model {
     public static final int FIELD_SELL_SIZE = 20;
     private EventListener eventListener;
+    public GameObjects gameObjects;
+    public int currentLevel = 1;
+    public LevelLoader levelLoader = new LevelLoader(Paths.get("..\\res\\levels.txt"));
 
     public void setEventListener(EventListener eventListener) {
         this.eventListener = eventListener;
+    }
+
+    public GameObjects getGameObjects() {
+        return gameObjects;
+    }
+
+    public void restartLevel(int level) {
+        this.gameObjects = levelLoader.getLevel(level);
+    }
+
+    public void restart() {
+        restartLevel(this.currentLevel);
+    }
+
+    public void startNextLevel() {
+        currentLevel++;
+        restartLevel(this.currentLevel);
     }
 }
