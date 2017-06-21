c4c3024b4d021eaac886a8cfd972f29177095866
astronarh <astronarh@gmail.com>
2017-06-21 17:59:04 +0300

    "ClassLoader - что это такое?" done

--- 4.JavaCollections/src/com/javarush/task/task35/task3507/Solution.java
+++ 4.JavaCollections/src/com/javarush/task/task35/task3507/Solution.java
@@ -1,16 +1,12 @@
 package com.javarush.task.task35.task3507;
 
-import com.javarush.task.task35.task3507.data.Cat;
-import com.javarush.task.task35.task3507.data.Sheep;
-import com.sun.org.apache.bcel.internal.util.ClassLoader;
-
-import java.io.*;
+import java.io.File;
+import java.io.FilenameFilter;
+import java.io.IOException;
 import java.lang.reflect.Constructor;
 import java.lang.reflect.Modifier;
-import java.net.URI;
-import java.nio.file.*;
+import java.nio.file.Files;
 import java.util.HashSet;
-import java.util.Iterator;
 import java.util.Set;
 
 /* 
@@ -23,207 +19,48 @@ public class Solution {
     }
 
     public static Set<? extends Animal> getAllAnimals(String pathToAnimals) {
-        Set<Animal> result = new HashSet<>();
-        File dir = new File(pathToAnimals);
-
-        Path path = new Path() {
+        Set<Animal> animals = new HashSet<>();
+        if (!pathToAnimals.endsWith("\\") && !pathToAnimals.endsWith("/")) pathToAnimals += "/";
+        final File f = new File(pathToAnimals);
+        File[] files = f.listFiles(new FilenameFilter() {
             @Override
-            public FileSystem getFileSystem() {
-                return null;
-            }
-
-            @Override
-            public boolean isAbsolute() {
-                return false;
-            }
-
-            @Override
-            public Path getRoot() {
-                return null;
-            }
-
-            @Override
-            public Path getFileName() {
-                return null;
-            }
-
-            @Override
-            public Path getParent() {
-                return null;
-            }
-
-            @Override
-            public int getNameCount() {
-                return 0;
-            }
-
-            @Override
-            public Path getName(int index) {
-                return null;
-            }
-
-            @Override
-            public Path subpath(int beginIndex, int endIndex) {
-                return null;
-            }
-
-            @Override
-            public boolean startsWith(Path other) {
-                return false;
-            }
-
-            @Override
-            public boolean startsWith(String other) {
-                return false;
-            }
-
-            @Override
-            public boolean endsWith(Path other) {
-                return false;
-            }
-
-            @Override
-            public boolean endsWith(String other) {
-                return false;
-            }
-
-            @Override
-            public Path normalize() {
-                return null;
-            }
-
-            @Override
-            public Path resolve(Path other) {
-                return null;
-            }
-
-            @Override
-            public Path resolve(String other) {
-                return null;
-            }
-
-            @Override
-            public Path resolveSibling(Path other) {
-                return null;
-            }
-
-            @Override
-            public Path resolveSibling(String other) {
-                return null;
-            }
-
-            @Override
-            public Path relativize(Path other) {
-                return null;
-            }
-
-            @Override
-            public URI toUri() {
-                return null;
-            }
-
-            @Override
-            public Path toAbsolutePath() {
-                return null;
-            }
-
-            @Override
-            public Path toRealPath(LinkOption... options) throws IOException {
-                return null;
-            }
-
-            @Override
-            public File toFile() {
-                return null;
-            }
-
-            @Override
-            public WatchKey register(WatchService watcher, WatchEvent.Kind<?>[] events, WatchEvent.Modifier... modifiers) throws IOException {
-                return null;
-            }
-
-            @Override
-            public WatchKey register(WatchService watcher, WatchEvent.Kind<?>[] events) throws IOException {
-                return null;
-            }
-
-            @Override
-            public Iterator<Path> iterator() {
-                return null;
-            }
-
-            @Override
-            public int compareTo(Path other) {
-                return 0;
-            }
-        };
-        File[] files = getClassFiles(path);
-
-        try {
-            for (File classFile : files) {
-                ClassLoader cl = getFileClassLoader(classFile);
-                String className = classFile.getName().replace(".class", "");
-                Class clazz = cl.loadClass(className);
-
-                boolean hasInterface = false;
-                Class[] interfaces = clazz.getInterfaces();
-                for (Class i : interfaces)
-                    if (Animal.class == i) {
-                        hasInterface = true;
-                        break;
-                    }
-                if (!hasInterface) continue;
-
-                boolean hasConstructor = false;
-                Constructor[] constructors = clazz.getConstructors();
-                for (Constructor c : constructors)
-                    if (Modifier.isPublic(c.getModifiers()) && c.getParameterTypes().length == 0) {
-                        hasConstructor = true;
-                        break;
-                    }
-                if (!hasConstructor) continue;
-
-                result.add((Animal) clazz.newInstance());
-            }
-        } catch (IllegalAccessException e) {
-            e.printStackTrace();
-        } catch (InstantiationException e) {
-            e.printStackTrace();
-        } catch (ClassNotFoundException e) {
-            e.printStackTrace();
-        }
-
-        return result;
-    }
-
-    public static File[] getClassFiles(Path path) {
-        File[] files = path.toFile().listFiles(new FileFilter() {
-            @Override
-            public boolean accept(File pathname) {
-                return pathname.getName().endsWith(".class");
+            public boolean accept(File dir, String name) {
+                return name.toLowerCase().endsWith(".class");
             }
         });
-        return files;
-    }
-
-    public static ClassLoader getFileClassLoader(final File classFile) {
-        return new ClassLoader() {
-            @Override
-            protected Class<?> findClass(String name) throws ClassNotFoundException {
-                byte[] bytes = getBytes(classFile);
-                return defineClass(null, bytes, 0, bytes.length);
+        ClassLoader classLoader;
+        for (File file : files) {
+            classLoader = new ClassLoader(){
+                @Override
+                protected Class<?> findClass(String name) throws ClassNotFoundException {
+                    try {
+                        byte[] buff = Files.readAllBytes(file.toPath());
+                        return defineClass(null, buff, 0, buff.length);
+                    } catch (IOException e) {
+                        return super.findClass(name);
+                    }
+                }
+            };
+            String shortName = file.getName().substring(0, file.getName().length() - 6);
+            try {
+                Class clazz = classLoader.loadClass(shortName);
+                Class[] interfaces = clazz.getInterfaces();
+                for (Class cl : interfaces) {
+                    if (cl.equals(Animal.class)){
+                        Constructor[] constructors = clazz.getConstructors();
+                        for (Constructor constr : constructors) {
+                            if (!Modifier.isAbstract(constr.getModifiers())
+                                    && Modifier.isPublic(constr.getModifiers())
+                                    && constr.getParameterCount() == 0){
+                                animals.add((Animal) clazz.newInstance());
+                            }
+                        }
+                    }
+                }
+            } catch (ClassNotFoundException | InstantiationException | IllegalAccessException e) {
+                e.printStackTrace();
             }
-        };
-    }
-
-
-    private static byte[] getBytes(File file) {
-        try {
-            return Files.readAllBytes(file.toPath());
-        } catch (IOException e) {
-            e.printStackTrace();
-            return null;
         }
+        return animals;
     }
 }
-
