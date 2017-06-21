13989a98206f201f9505917538e342db114a4cb6
astronarh <astronarh@gmail.com>
2017-06-21 16:00:25 +0300

    "Добавление файла в архив" done

---
+++ 4.JavaCollections/src/com/javarush/task/task31/task3105/Solution.java
@@ -0,0 +1,42 @@
+package com.javarush.task.task31.task3105;
+
+import java.io.*;
+import java.nio.file.Files;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
+import java.util.zip.ZipOutputStream;
+
+/* 
+Добавление файла в архив
+*/
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        String fileName = args[0];
+        String zipFileName = args[1];
+        File file = new File(fileName);
+        Map<String, ByteArrayOutputStream> archivedFiles = new HashMap<>();
+        try (ZipInputStream zipInputStream = new ZipInputStream(new FileInputStream(zipFileName))) {
+            ZipEntry zipEntry;
+            while ((zipEntry = zipInputStream.getNextEntry()) != null) {
+                ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+                byte[] buffer = new byte[1024];
+                int count = 0;
+                while ((count = zipInputStream.read(buffer)) != -1) byteArrayOutputStream.write(buffer, 0, count);
+                archivedFiles.put(zipEntry.getName(), byteArrayOutputStream);
+            }
+        }
+        try (ZipOutputStream zipOutputStream = new ZipOutputStream(new FileOutputStream(zipFileName))) {
+            for (Map.Entry<String, ByteArrayOutputStream> pair : archivedFiles.entrySet()) {
+                if (pair.getKey().substring(pair.getKey().lastIndexOf("/") + 1).equals(file.getName())) continue;
+                zipOutputStream.putNextEntry(new ZipEntry(pair.getKey()));
+                zipOutputStream.write(pair.getValue().toByteArray());
+            }
+
+            ZipEntry zipEntry = new ZipEntry("new/" + file.getName());
+            zipOutputStream.putNextEntry(zipEntry);
+            Files.copy(file.toPath(), zipOutputStream);
+        }
+    }
+}
