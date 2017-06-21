bbf2a27234a28188d4cc8ea20da9e724ff5eb166
astronarh <astronarh@gmail.com>
2017-06-21 11:42:29 +0300

    "Sokoban (12)" done

--- 4.JavaCollections/src/com/javarush/task/task34/task3410/view/Field.java
+++ 4.JavaCollections/src/com/javarush/task/task34/task3410/view/Field.java
@@ -1,9 +1,11 @@
 package com.javarush.task.task34.task3410.view;
 
 import com.javarush.task.task34.task3410.controller.EventListener;
+import com.javarush.task.task34.task3410.model.GameObject;
 
 import javax.swing.*;
 import java.awt.*;
+import java.util.Set;
 
 /**
  * Created by ShkerdinVA on 20.06.2017.
@@ -17,7 +19,10 @@ public class Field extends JPanel {
     }
 
     public void paint(Graphics g) {
-
+        g.setColor(Color.BLACK);
+        g.fillRect(0, 0, 300, 300);
+        Set<GameObject> gameObjectSet = view.getGameObjects().getAll();
+        for (GameObject gameObject : gameObjectSet) gameObject.draw(g);
     }
 
     public void setEventListener(EventListener eventListener) {
